9be64d194c257440bee9761a39cd17f921a40616
MartinHurnak <hurnak.m@gmail.com>
2025-12-11 14:12:03 +0100

    Day 11 Part 2

--- 11/2.py
+++ 11/2.py
@@ -22,27 +22,26 @@ def main(filename):
         for output in outputs:
             G.add_edge(node, output)
 
-
-    print(G)
-    fft_nodes = nx.ancestors(G, "fft") | nx.descendants(G, "fft") | {"fft"}
-    dac_nodes = nx.ancestors(G, "dac") | nx.descendants(G, "dac") | {"dac"}
-    print("dac" in nx.ancestors(G, "fft"), "dac" in nx.descendants(G, "fft"))
-
-    svr_fft_G = G.subgraph(nx.ancestors(G, "fft") | {"fft"})
+    # fft and dac need to be in specific order, otherwise graph would not be DAG
+    # based on ancestors/descendants it needs to be fft->dac
+    # split the graph into 3 parts (svr->fft, fft->dac, dac->out)
+    # then reconstructing it will keep only svr->out paths going through fft and dac
+    svr_fft_G = G.subgraph((nx.ancestors(G, "fft")) | {"fft"} )
     fft_dac_G = G.subgraph((nx.descendants(G, "fft") & nx.ancestors(G, "dac"))|{"fft", "dac"})
-    dac_out_G = G.subgraph(nx.descendants(G, "dac") | {"dac"})
-    print(svr_fft_G)
-    print(fft_dac_G)
-    print(dac_out_G)
+    dac_out_G = G.subgraph((nx.descendants(G, "dac"))| {"dac"})
+    G = G.edge_subgraph(svr_fft_G.edges | fft_dac_G.edges | dac_out_G.edges)
 
-    svr_fft = list(nx.all_simple_paths(G, source="svr", target="fft"))
-    print("svr_fft", svr_fft[0])
-    fft_dac = list(nx.all_simple_paths(G, source="fft", target="dac"))
-    print("fft_dac", fft_dac[0])
-    dac_out = list(nx.all_simple_paths(G, source="dac", target="out"))
-    print("dac_out", dac_out[0])
-    return len(svr_fft) * len(fft_dac) * len(dac_out)
+    # nx.all_simple_paths seems too tedious to iterate through all combinations
+    # we are only interested in number
+    # each node has that many paths going into it from svr that is the sum of it's predecessors's paths
+    paths = {"svr": 1}
+    for node in nx.topological_sort(G):
+        if node == "svr": 
+            continue
+        # thanks to topological sort, we can rely on predecessors being resolved
+        paths[node] = sum(paths[predecessor] for predecessor in G.predecessors(node))
+    return paths["out"]
 
 
-EXPECTED_TEST_RESULT = 2 # TODO change based on test
+EXPECTED_TEST_RESULT = 2 
 test_and_run(main, testfile, EXPECTED_TEST_RESULT, inputfile)
